0b02106666eac5fdca98731ec153605fee37b098
RandikaSilva <mlasitharandika@gmail.com>
2025-04-30 00:19:16 +1200

    Forecasting changes

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from src.controller import consumption_forecasting
+from src.controller import consumption_forecasting, carban_emissions_forcasting
