10d09831741204f73ad00cebccabda3a8371b34a
RandikaSilva <mlasitharandika@gmail.com>
2025-04-30 21:30:27 +1200

    Demand Vs Generation Changes

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from src.controller import consumption_forecasting, carban_emissions_forcasting
+from src.controller import consumption_forecasting, carban_emissions_forcasting, demand_vs_generation
